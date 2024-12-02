5747eae76c9e5e1b7175ec923da199898f13dec9
CHAYAN PAL <120254873+palC02@users.noreply.github.com>
2024-12-02 23:55:40 +0530

    Create OpenMP_1.c

---
+++ OpenMp/OpenMP_1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<omp.h>
+
+int x=5;
+
+first(){
+x++;
+printf("\nx=%d from first");
+}
+
+second(){
+x--;
+printf("\nx=%d from second");
+}
+
+int main(){
+  #pragma omp parallel sections
+{
+    #pragma omp section
+      first();
+    #pragma omp section
+      second();
+}
+printf("\nx=%d at end",x);
+}
